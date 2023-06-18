b063b672df938e4b4e76227c3d6df3e75f3e53e7
audrey <amonaaudrey16@gmail.com>
2023-06-18 12:31:30 +0000

    création du fichier __init__.py pour modeles

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
-from table_classe import TableClasse
-from table_eleve import TableEleve
-from table_enseignant import TableEnseignant
-from table_matiere import TableMatiere
-from table_niveau_serie import TableNiveauSerie
-from table_programme import TableProgramme
-from table_tuteur import TableTuteur
+from modeles.table_classe import TableClasse
+from modeles.table_eleve import TableEleve
+from modeles.table_enseignant import TableEnseignant
+from modeles.table_matiere import TableMatiere
+from modeles.table_niveau_serie import TableNiveauSerie
+from modeles.table_programme import TableProgramme
+from modeles.table_tuteur import TableTuteur
 
 
 class Database(object):
---
+++ modeles/__init__.py
@@ -0,0 +1,8 @@
+import modeles.table 
+import  modeles.table_classe
+import modeles.table_eleve
+import modeles.table_enseignant
+import modeles.table_matiere
+import modeles.table_niveau_serie
+import modeles.table_programme
+import modeles.table_tuteur
