b7458e9dfb5aaec4342fc553438a60fc9b3524ad
audrey <amonaaudrey16@gmail.com>
2023-06-20 15:16:19 +0000

    creation d'un update abstrait dan Table

--- routes/controllers/modeles/table_tuteur.py
+++ routes/controllers/modeles/table_tuteur.py
@@ -3,33 +3,45 @@ from .table_personne import TablePersonne
 
 
 class TableTuteur(TablePersonne):
-    relation = 'tuteur'
-    schema = [("matricule", "k", "serial", "", ""),
-              ("nom", "", "varchar", "", ""),
-              ("prenoms", "", "varchar", "", ""),
-              ("date_naissance", "", "date", "", ""),
-              ("contact", "", "varchar", "", ""),
-              ("genre", "", "varchar", "", ""),
-              ("adresse", "", "varchar", "", ""),
-              ("profession", "", "varchar", "", "")]
-    primary_key = 'matricule'
+    relation = "tuteur"
+    schema = [
+        ("matricule", "k", "serial", "", ""),
+        ("nom", "", "varchar", "", ""),
+        ("prenoms", "", "varchar", "", ""),
+        ("date_naissance", "", "date", "", ""),
+        ("contact", "", "varchar", "", ""),
+        ("genre", "", "varchar", "", ""),
+        ("adresse", "", "varchar", "", ""),
+        ("profession", "", "varchar", "", ""),
+    ]
+    primary_key = "matricule"
 
-    def __init__(self,  nom=None, prenom=None, date_naissance=None, contact=None, genre=None, adresse=None, profession=None):
+    def __init__(
+        self,
+        nom=None,
+        prenom=None,
+        date_naissance=None,
+        contact=None,
+        genre=None,
+        adresse=None,
+        profession=None,
+    ):
         TablePersonne.__init__(
-            self,  nom, prenom, date_naissance, contact, genre, adresse)
+            self, nom, prenom, date_naissance, contact, genre, adresse
+        )
         self.profession = profession
-
 
     @classmethod
     def update_profession(cls, matricule, profession):
-        row = ''
+        row = ""
         try:
             req = f"UPDATE  {cls.relation} SET profession = '{profession}' WHERE {cls.primary_key} = '{matricule}';"
             cls.lk.executerReq(req)
             row = cls.select_attr_where_id(cls.INFO_ATTR, matricule)
         except Exception as err:
             print(
-                f"Une erreur est surmenu lors de la mise à jour de la profession de  {row[0]} {row[1]}:\n{req}\n :")
+                f"Une erreur est surmenu lors de la mise à jour de la profession de  {row[0]} {row[1]}:\n{req}\n :"
+            )
             print(err)
             return 0
         else:
@@ -38,35 +50,16 @@ class TableTuteur(TablePersonne):
 
 
 def main():
-
     datenaiss = dt.datetime(2003, 1, 2).date()
-    tut = TableTuteur("Dosseh", "OOO", datenaiss, "70546987",
-                      "M", "BP 25 Sok", "Soulard")
+    tut = TableTuteur(
+        "Dosseh", "OOO", datenaiss, "70546987", "M", "BP 25 Sok", "Soulard"
+    )
     tut.create()
-    tut.updateProfession(2, "Koba")
+    tut.update(2, "profession", "Koba")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    print('5555')
+    print("5555")
     pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
