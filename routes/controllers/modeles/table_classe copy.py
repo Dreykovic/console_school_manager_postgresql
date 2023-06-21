81d1bdb1e9bb2bd967c5b0187ee1feabf30f8c20
audrey <amonaaudrey16@gmail.com>
2023-06-21 17:37:27 +0000

    transformation du schema de Classe en dict

--- routes/controllers/modeles/table_classe copy.py
+++ routes/controllers/modeles/table_classe copy.py
@@ -8,25 +8,6 @@ class TableClasse(Table):
         ("nom", "", "varchar", "", ""),
         ("effectif", "", "integer", "", ""),
     ]
-    schema = [{
-        'column_name': 'id_classe',
-        'constraint': 'pk',
-        'type': 'serial',
-        'reference_col': '',
-        'reference_table': ''
-    }, {
-        'column_name': 'nom',
-        'constraint': '',
-        'type': 'varchar',
-        'reference_col': '',
-        'reference_table': ''
-    }, {
-        'column_name': 'effectif',
-        'constraint': '',
-        'type': 'integer',
-        'reference_col': '',
-        'reference_table': ''
-    }]
     primary_key = "id_classe"
 
     def __init__(self, nom=None, effectif=0):
