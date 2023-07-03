eccded77418190097418ca16021e94b7c736a7e6
audrey <amonaaudrey16@gmail.com>
2023-07-03 05:25:30 +0000

    ajout de trigger

--- controllers/make_migration.py
+++ controllers/make_migration.py
@@ -1,19 +1,29 @@
-from .modeles.table_classe import TableClasse
-from .modeles.table_eleve import TableEleve
-from .modeles.table_enseignant import TableEnseignant
-from .modeles.table_matiere import TableMatiere
-from .modeles.table_programme import TableProgramme
-from .modeles.table_tuteur import TableTuteur
-from .datetime import datetime as dt
-from .modeles.migration import *
-from .modeles.database import tables
+from modeles.table_classe import TableClasse
+from modeles.table_eleve import TableEleve
+from modeles.table_enseignant import TableEnseignant
+from modeles.table_matiere import TableMatiere
+from modeles.table_programme import TableProgramme
+from modeles.table_tuteur import TableTuteur
+from datetime import datetime as dt
+from modeles.migration import *
+from modeles.database import tables
+from modeles.linker import Linker
 
-for relation in tables:
-    create_tables(relation, tables[relation])
+
+# for relation in tables:
+#     create_tables(relation, tables[relation])
 
 
 TableClasse("2nde A4").create()
 TableClasse("2nde D").create()
+TableClasse("2nde C4").create()
+TableClasse("2nde C4").create()
+TableClasse("1ere D").create()
+TableClasse("1ere A4").create()
+TableClasse("1ere C4").create()
+TableClasse("2nde A4").create()
+TableClasse("2nde D").create()
+TableClasse("2nde C4").create()
 TableClasse("2nde C4").create()
 TableClasse("1ere D").create()
 TableClasse("1ere A4").create()
